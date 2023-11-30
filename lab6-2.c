a269a56e45690dcb87b005afb1bf342cb003c754
Hasan Kılınç <119063341+hasankilincce@users.noreply.github.com>
2023-11-30 08:35:57 +0300

    Update lab6-2.c

--- lab6-2.c
+++ lab6-2.c
@@ -5,20 +5,29 @@ int main() {
     printf("How many prime numbers to be listed: ");
     scanf("%d", &num);
 
-    while(counter<num) {
-        while (k <= x) {
-            if (x % k == 0) 
-            {bolen += 1;
+    while(0==0){
+        while(counter<num) {
+            while (k <= x) {
+                if (x % k == 0)
+                {bolen += 1;
+                }
+                k += 1;
             }
-            k += 1;
+            if (bolen == 2) {
+                printf("%d ", x);
+                counter += 1;
+            }
+            bolen = 0;
+            k = 1;
+            x += 1;
         }
-        if (bolen == 2) {
-            printf("%d ", x);
-            counter += 1;
+
+        printf("\nHow many prime numbers to be listed: ");
+        scanf("%d", &num);
+        x=2, k=1, bolen=0, counter=0;
+        if(num == -1){
+            break;
         }
-        bolen = 0;
-        k = 1;
-        x += 1;
     }
     return 0;
 }
